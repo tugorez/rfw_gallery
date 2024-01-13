c6cd0daaf6435e7671a7aeb4ab0a86bbd682b04a
Juanjo Tugores <tugorez@gmail.com>
2024-01-13 07:33:31 -0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/rfw_gallery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rfw_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:rfw_gallery/app.dart';
+
+void main() {
+  runApp(const App());
+}
